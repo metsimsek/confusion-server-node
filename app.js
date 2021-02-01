b94de643e96db1fbf9dee6c06668c6df0d3ecd24
metsimsek <metsimsek@gmail.com>
2021-02-01 01:19:43 +0000

    Passport for authentication

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-//var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+
+var path = require('path');
+var createError = require('http-errors');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,8 +14,6 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
 
 var app = express();
 
@@ -22,7 +24,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(session({
   name: 'session-id',
@@ -32,26 +33,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
