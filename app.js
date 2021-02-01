364aac264eeb1ad66cac54d6d8354be6219a5b4b
metsimsek <metsimsek@gmail.com>
2021-02-01 03:41:43 +0000

    Passport JWT

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var logger = require('morgan');
-var session = require('express-session');
 
 var path = require('path');
 var createError = require('http-errors');
-var FileStore = require('session-file-store')(session);
 var passport = require('passport');
-var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-
+var config = require('./config');
+const url = config.mongoUrl;
 
 var app = express();
 
@@ -25,34 +23,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -77,7 +52,6 @@ app.use(function(err, req, res, next) {
 });
 
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
